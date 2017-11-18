8ae12a589b767f59db73e362837cb054649b4dc1
Mikkel1996DK <Mikkelljungberg@gmail.com>
2017-11-18 20:54:26 +0100

    Added the CreateScheduleControl.
    Added a *very* important comment in MainWindow.xaml.cs.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,6 +23,11 @@ namespace YoungEnterprise_Admin_Application
         //TODO: Have a look at this in order to check how to generate unique passwords for users!
         // https://stackoverflow.com/questions/54991/generating-random-passwords
         // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+        // All of the xaml "main"-grids RowDefinitions should:
+        // - All should add up to 9
+        // - Top two rows should be of the auto-size of 1.5* except if the usercontrol contains a DataGrid
+        //   Such as the CreateScheduleControl's layout
+        //   and also the ViewUsersControl's layout
 
         public MainWindow()
         {
