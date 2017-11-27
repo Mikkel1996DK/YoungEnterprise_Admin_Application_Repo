796d24608598dbf90670403b4cedebddbe7c107a
Mikkel1996DK <Mikkelljungberg@gmail.com>
2017-11-27 13:08:18 +0100

    Added HashPassword Test under UserServicesTest. Added UserService, used to handling the users on the platform.

---
+++ ServiceTest/UserServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceTest
+{
+    [TestClass]
+    public class UserServiceTest
+    {
+        [TestMethod]
+        public void TestHashPassword()
+        {
+            //e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855 = "TESTPASSWORD"
+            Assert.AreEqual("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855", HashPassword("TESTPASSWORD"));
+        }
+
+    }
+}
